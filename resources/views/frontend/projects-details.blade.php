4e14873b7b2ee8310ae94c1df9cad687804748e0
Mohammad Alamin <mohammadalamin.dev@gmail.com>
2024-04-03 10:38:34 +0600

    project detail page modify

--- resources/views/frontend/projects-details.blade.php
+++ resources/views/frontend/projects-details.blade.php
@@ -1,37 +1,84 @@
 @extends('app')
 @section('content')
+    <style>
+        .carousel-indicators [data-bs-target] {
+            width: 19px;
+            height: 5px;
+            background-color: #00d4ff;
+            background-clip: padding-box;
+            border: 0;
+            transition: opacity .6s ease;
+            border-radius: 6px !important;
+        }
+
+        .contact-form img {
+            right: 30px;
+            width: 32px;
+        }
+
+        .social-links li a {
+            width: 75px;
+            height: 75px;
+            line-height: 80px;
+        }
+    </style>
     <section class="py-5">
         <div class="container">
             <div class="row">
                 <div class="col-lg-7">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
                         <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            @foreach ($projectData->images as $index => $image)
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{ $index }}" class="{{ $loop->first ? 'active' : '' }}" aria-label="Slide {{ $index + 1 }}"></button>
+                            @endforeach
                         </div>
                         <div class="carousel-inner">
                             @foreach ($projectData->images as $image)
-                                <div class="carousel-item {{ $loop->first ? 'active' : '' }} " style="background: #000">
-                                    <img src="{{ asset($image->image_path) }}" class="d-block w-100 h-100 object-cover" alt="...">
+                                <div class="carousel-item {{ $loop->first ? 'active' : '' }}  w-100" style="background: #000; height:325px; border-radius: 30px;">
+                                    <img src="{{ asset($image->image_path) }}" class="d-block w-100 h-100 object-cover" style="border-radius: 30px;" alt="...">
                                 </div>
                             @endforeach
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
+                            <i class="iconoir-fast-arrow-left fs-2" style="color: #00d4ff"></i>
                         </button>
                         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
+                            <i class="iconoir-fast-arrow-right fs-2" style="color: #00d4ff"></i>
                         </button>
                     </div>
                 </div>
 
                 <div data-aos="zoom-in" class="contact-form col-lg-5">
-                    <div class="shadow-box py-5 px-4">
+                    <div class="shadow-box p-4">
                         <img decoding="async" src="{{ asset('img/icon2.png') }}" alt="Star" class="star-icon">
-                        <h4 class="mb-3">GET IN <span>TOUCH !</h4>
+                        <div class="mb-3">
+                            <h5 class="mb-3" data-aos="fade-up" class="aos-init aos-animate">PROJECT <span>LINK !</h5>
+                            <ul class="social-links d-flex align-center justify-content-center pt-2 aos-init aos-animate" data-aos="zoom-in">
+                                <li>
+                                    <a href="https://www.facebook.com/mohammadalamin.dev" target="_blank" class="shadow-box">
+                                        <i class="iconoir-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/in/mdalamin1998/" target="_blank" class="shadow-box">
+                                        <i class="iconoir-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/alaminwebdev" target="_blank" class="shadow-box">
+                                        <i class="iconoir-github"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mohammadalamin.dev@gmail.com" target="_blank" class="shadow-box">
+                                        <i class="iconoir-mail"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5 data-aos="fade-up" class="aos-init aos-animate pb-2">TAGS</h5>
+                        </div>
                     </div>
                 </div>
 
